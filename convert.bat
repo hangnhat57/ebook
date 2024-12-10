bfe5ec2d00910077b22df3a43cff4edd5a829995
Nathan <hangnhat57@gmail.com>
2024-12-10 19:01:40 +0700

    Fix CSS selector duplication for h2 elements and update pandoc command to use base.css

--- convert.bat
+++ convert.bat
@@ -1,7 +1,7 @@
 pandoc -o deep-learning.epub .\book.md ^
   --resource-path="./images" ^
   --mathjax ^
-  --css=style.css ^
+  --css=base.css ^
   --webtex ^
   --highlight-style=pygments ^
   --epub-cover-image=cover.png ^
